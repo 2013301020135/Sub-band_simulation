1edb2520aa1b8947b35b3dfacc314986f1988334
Yang Liu <gottfried.moriarty@outlook.com>
2025-04-17 10:28:31 +0800

    Comment the JUMP and TN parameters in posttn par files

---
+++ comment_posttn.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""Tidy up posttn par files by commenting out all JUMP and TN parameters. Written by Yang Liu (liuyang@shao.ac.cn)."""
+
+import argparse
+import os
+import glob
+
+parser = argparse.ArgumentParser(description='Tidy up posttn par files by commenting out all JUMP and TN parameters.'
+                                             'Written by Yang Liu (liuyang@shao.ac.cn).')
+parser.add_argument('-p', '--parfile', type=str, default=[], nargs='+',
+                    help='Parameter files for pulsars used in simulation')
+parser.add_argument('-d', '--datadir', type=str, default=None, help='Path to the directory containing the par files')
+
+
+def comment_posttn(parfile):
+    """Comment out the JUMP and TN parameters in the posttn parfiles."""
+    if "-posttn" in parfile:
+        parlines = []
+        with open(parfile) as f:
+            for line in f:
+                e = line.split()
+                if "JUMP" in e[0] or "TN" in e[0] or "DM_SERIES" in e[0]:
+                    parlines.append("# "+line)
+                else:
+                    parlines.append(line)
+        newpar = parfile.split("-posttn")[0]+".par"
+        with open(newpar, "w") as newf:
+            newf.writelines(parlines)
+        os.remove(parfile)
+        return newpar
+    else:
+        return parfile
+
+
+args = parser.parse_args()
+if args.datadir is not None:
+    posttn_files = sorted(glob.glob(os.path.join(args.datadir, "*.par")))
+    par_files = sorted(comment_posttn(pfs) for pfs in posttn_files)
+else:
+    par_files = sorted(comment_posttn(pfs) for pfs in args.parfile)
